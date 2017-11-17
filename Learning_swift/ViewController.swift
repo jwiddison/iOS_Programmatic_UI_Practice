9038fe09ff78ec2707affa9dbf3f2e96191a387c
Jordan Widdison <jordan.widdison@gmail.com>
2017-11-16 23:56:05 -0700

    Initial Commit

---
+++ Learning_swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Learning_swift
+//
+//  Created by Jordan Widdison on 11/16/17.
+//  Copyright © 2017 Jordan Widdison. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
